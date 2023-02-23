43502a0a56f229cb536a0e0f47a3258c9d4140f1
KevinBacklund <kebacklund@gmail.com>
2023-02-23 13:38:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lander
+{
+    internal class Program
+    {
+        public class Land
+        {
+            public string namn = "";
+            public string styrestyp = "";
+            public string huvudstad = "";
+            public string invånarantal = "";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
