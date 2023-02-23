ada17e5e53a2d733543646ec2f2a313483cf81e5
KevinBacklund <kebacklund@gmail.com>
2023-02-23 14:29:46 +0100

    Skapat array för länder

--- Program.cs
+++ Program.cs
@@ -39,6 +39,39 @@
             sverige.Print();
             tyskland.Print();
             sanMarino.Print();
+ 
+            Land[] landarr = new Land[7];
+            landarr[0] = sverige;
+            landarr[1] = tyskland;
+            landarr[2] = sanMarino;
+            landarr[3] = new Land()
+            {
+                namn = "Danmark",
+                styrestyp = "monarki",
+                huvudstad = "Köpenhamn",
+                invånarantal = "5928364"
+            };
+            landarr[4] = new Land()
+            {
+                namn = "Italien",
+                styrestyp = "republik",
+                huvudstad = "Rom",
+                invånarantal = "58853482"
+            };
+            landarr[5] = new Land()
+            {
+                namn = "Tjeckien",
+                styrestyp = "republik",
+                huvudstad = "Prag",
+                invånarantal = "10551219"
+            };
+            landarr[6] = new Land()
+            {
+                namn = "Rumänien",
+                styrestyp = "republik",
+                huvudstad = "Bukarest",
+                invånarantal = "19760314"
+            };
         }
     }
 }
